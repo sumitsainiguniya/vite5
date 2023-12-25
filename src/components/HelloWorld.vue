74b49d93b5f1699dc86531d66a94d8b1f24bd846
Sumit Saini <sumitsaini0022@gmail.com>
2023-12-25 21:37:03 +0530

    added pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <template>
   <h1>{{ msg }}</h1>
   <div class="card">
-    <button type="button" @click="count++">Release {{ count }}</button>
+    <button type="button" @click="store.increment()">Release {{ count }}</button>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
-
+import { computed } from 'vue'
+import { useCounterStore } from '../stores/counter'
 defineProps<{ msg: string }>()
-
-const count = ref(1)
+const store = useCounterStore()
+const count = computed(() => store.count)
 </script>
 <style scoped>
 .read-the-docs {
