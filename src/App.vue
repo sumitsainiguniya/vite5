192881b02e0fe64d8462d524ddd463ee50e8fe79
Sumit Saini <sumitsaini0022@gmail.com>
2023-12-25 21:40:27 +0530

    added in msg pinia

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
-const msg = ref('Vite + Vue')
+const msg = ref('Vite + Vue + Pinia')
 </script>
 
 <template>
