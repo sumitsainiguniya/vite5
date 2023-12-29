07402ca65fdcd0899d98430912472de09ba91bf0
Sumit Saini <44585428+sumitsainiguniya@users.noreply.github.com>
2023-12-29 10:23:24 +0000

    fixed sort url ts and vite config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
-import App from "./App.vue";
+import App from "@/App.vue";
 
 const pinia = createPinia();
 const app = createApp(App);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vite5/'
-})
+  base: "/vite5/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, 'src/'),
+    },
+  },
+});
